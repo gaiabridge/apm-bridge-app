d835f5e2c1c1adc4e6ef4f403e617893424a2cd6
Sim Young Jae <hanul@hanul.me>
2022-05-18 21:05:17 +0900

    dev

--- src/klaytn/Klaytn.ts
+++ src/klaytn/Klaytn.ts
@@ -1,13 +1,6 @@
 class Klaytn {
 
-    public caver = new (window as any).Caver(new (window as any).Caver.providers.WebsocketProvider("wss://klaytn-node.klu.bs:9091", {
-        reconnect: {
-            auto: true,
-            delay: 1000,
-            maxAttempts: true,
-            onTimeout: false
-        },
-    }));
+    public caver = new (window as any).Caver("https://en.apm-coin.com:8551");
 
     public createContract(address: string, abi: any) {
         return this.caver.contract.create(abi, address);
