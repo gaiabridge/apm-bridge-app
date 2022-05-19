a2af19fab4231141bb20bd51f2e5ba09782a878d
jeon <46668752+john-ver@users.noreply.github.com>
2022-05-19 14:21:59 +0900

    Change en

--- src/klaytn/Klaytn.ts
+++ src/klaytn/Klaytn.ts
@@ -1,6 +1,6 @@
 class Klaytn {
 
-    public caver = new (window as any).Caver("https://en.apm-coin.com:8551");
+    public caver = new (window as any).Caver("https://en.apm-coin.com");
 
     public createContract(address: string, abi: any) {
         return this.caver.contract.create(abi, address);
