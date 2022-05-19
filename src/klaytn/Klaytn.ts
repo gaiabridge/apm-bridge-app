66aa768049631057130232be1fc3cbffecc121bd
jeon <46668752+john-ver@users.noreply.github.com>
2022-05-19 18:43:39 +0900

    public en wss 임시적용

--- src/klaytn/Klaytn.ts
+++ src/klaytn/Klaytn.ts
@@ -1,6 +1,13 @@
 class Klaytn {
 
-    public caver = new (window as any).Caver("https://en.apm-coin.com");
+    public caver = new (window as any).Caver(new (window as any).Caver.providers.WebsocketProvider("wss://public-node-api.klaytnapi.com/v1/cypress/ws", {
+        reconnect: {
+            auto: true,
+            delay: 1000,
+            maxAttempts: true,
+            onTimeout: false
+        },
+    }));
 
     public createContract(address: string, abi: any) {
         return this.caver.contract.create(abi, address);
