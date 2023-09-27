48701bac8bf0d762ebe153724f3026917e374251
jeon <46668752+john-ver@users.noreply.github.com>
2023-09-27 15:30:10 +0900

    node endpoint 교체

--- src/klaytn/Klaytn.ts
+++ src/klaytn/Klaytn.ts
@@ -1,6 +1,6 @@
 class Klaytn {
 
-    public caver = new (window as any).Caver("https://public-node-api.klaytnapi.com/v1/cypress");
+    public caver = new (window as any).Caver("https://public-en-cypress.klaytn.net");
 
     public createContract(address: string, abi: any) {
         return this.caver.contract.create(abi, address);
